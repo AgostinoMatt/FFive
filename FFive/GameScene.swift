f24e36957984828793f047fc4e0daba158cd62b5
Alexx Quan <alexxwithtwoxx@gmail.com>
2018-06-15 14:08:43 -0400

    added code

--- FFive/GameScene.swift
+++ FFive/GameScene.swift
@@ -7,85 +7,90 @@
 //
 
 import SpriteKit
-import GameplayKit
 
 class GameScene: SKScene {
     
-    private var bestBagelInTheWorld = 0
+    let characterOfMain = SKSpriteNode(fileNamed: "hero1fight")
+    let characterOfMainMovePointsPerSec: CGFloat = 480.0
+    let characterOfMainRotateRadiansPerSec: CGFloat = 4.0 * π // alt-p makes π
+    let characterOfMainAnimation: SKAction
+    var velocity = CGPoint.zero
+    var lastUpdateTime: TimeInterval = 0
+    var deltaTime: TimeInterval = 0
+    var lastTouchLocation: CGPoint?
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    let playableRect: CGRect
+    
+    let cameraNode = SKCameraNode()
+    let cameraMovePointPerSec: CGFloat = 200.0
+    var cameraRect: CGRect {
+        let x = cameraNode.position.x - size.width / 2 + (size.width - //current pos subtract half of screen width
+            playableRect.width) / 2
+        let y = cameraNode.position.y - size.height / 2 + (size.height - // add half of scren width
+            playableRect.height) / 2
+        
+        return CGRect(x: x, y: y, width: playableRect.width, height:
+            playableRect.height)
+    }
+    
+    override init(size: CGSize) {
+        let maxAspectRatio: CGFloat = 16.0/9.0
+        let playableHeight = size.width / maxAspectRatio
+        let playableMargin = (size.height - playableHeight) / 2.0
+        playableRect = CGRect(x: 0, y: playableMargin, width: size.width, height: playableHeight)
+        
+        var textures: [SKTexture] = []
+        
+        /*for i in 1...4 {
+            textures.append(SKTexture(imageNamed: "zombie\(i)"))
+        }
+       textures.append(textures[2])
+        textures.append(textures[1])
+        
+        zombieAnimation = SKAction.animate(with: textures, timePerFrame: 0.1)*/
+        super.init(size: size)
+    }
+    
+    required init(coder aDecoder: NSCoder){
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     
     override func didMove(to view: SKView) {
+        backgroundColor = SKColor.black
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        /*for i in 0...1 {
+            let background = backgroundNode()
+            //background.anchorPoint = CGPoint.zero //CGPoint(x: 0.5, y: 0.5)
+            //background.position = CGPoint.zero //CGPoint(x: size.width / 2, y: size.height / 2)
+            background.position = CGPoint(x: CGFloat(i) *
+                background.size.width, y: 0)
+            background.zPosition = -1
+            addChild(background)
+        }*/
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        characterOfMain.position = CGPoint(x: 400, y: 400)
+        //zombie.run(SKAction.repeatForever(zombieAnimation))
+        startcharacterOfMainAnimation()
+        addChild(characterOfMain)
+        //spawnEnemy()
+        run(SKAction.repeatForever(
+            SKAction.sequence([SKAction.run() { [unowned self] in
+               // self.spawnEnemy()
+                }, SKAction.wait(forDuration: 2.0)])))
+        run(SKAction.repeatForever(
+            SKAction.sequence([SKAction.run() { [unowned self] in
+                // self.spawnCat()
+                }, SKAction.wait(forDuration: 1.0)])))
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+        //playBackgroundMusic(filename: "backgroundMusic.mp3")
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        addChild(cameraNode)
+        camera = cameraNode
+        cameraNode.position = CGPoint(x: size.width / 2, y: size.height / 2)
     }
     
     
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
+    
+    
 }
