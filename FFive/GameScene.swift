f9a3cef9d38fd1a23152f5b5155cd7e68b6b99f9
matt <Agostino_matt@hotmail.com>
2018-06-19 14:15:28 -0400

    clean up

--- FFive/GameScene.swift
+++ FFive/GameScene.swift
@@ -9,54 +9,6 @@
 import SpriteKit
 
 class GameScene: SKScene {
-    
-
-   //var background: SKTileMapNode!
-    //var player = Player()
-    
-    /*
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        background =
-            childNode(withName: "background") as! SKTileMapNode
-    }*/
-/*
-    let characterOfMain:SKSpriteNode
-    let characterOfMainMovePointsPerSec: CGFloat = 480.0
-    let characterOfMainRotateRadiansPerSec: CGFloat = 4.0 * π // alt-p makes π
-    let characterOfMainAnimation: SKAction
-    var velocity = CGPoint.zero
-    var lastUpdateTime: TimeInterval = 0
-    var deltaTime: TimeInterval = 0
-    var lastTouchLocation: CGPoint?
-    
-    
-    
-    
-    override func didMove(to view: SKView) {
-        addChild(player)
-        
-    override init(size: CGSize) {
-        let maxAspectRatio: CGFloat = 16.0/9.0
-        let playableHeight = size.width / maxAspectRatio
-        let playableMargin = (size.height - playableHeight) / 2.0
-        playableRect = CGRect(x: 0, y: playableMargin, width: size.width, height: playableHeight)
-        
-        var textures: [SKTexture] = []
-        
-        for i in 1...4 {
-            textures.append(SKTexture(imageNamed: "hero\(i)"))
-        }
-       textures.append(textures[2])
-        textures.append(textures[1])
-        
-        characterOfMain =  SKSpriteNode(imageNamed: "hero1fight")
-        
-        //zombieAnimation = SKAction.animate(with: textures, timePerFrame: 0.1)
-        super.init(size: size)
-    }
-}*/
-
 
 
     var characterOfMain: SKSpriteNode!
@@ -94,8 +46,6 @@ class GameScene: SKScene {
         
         characterOfMain = SKSpriteNode(imageNamed: "hero8")
         
-        //fatalError("init(coder:) has not been implemented")
-        
     }
     
 
@@ -129,7 +79,6 @@ class GameScene: SKScene {
                 }
                 else {
                     move(sprite: characterOfMain, velocity: velocity)
-                    rotate(sprite: characterOfMain, direction: velocity, rotateRadiansPerSec: characterOfMainRotateRadiansPerSec)
                 }
             }
      
@@ -145,12 +94,6 @@ class GameScene: SKScene {
         func move(sprite: SKSpriteNode, velocity: CGPoint) {
             let amountToMove = velocity * CGFloat(deltaTime)
             sprite.position += amountToMove
-        }
-     
-        func rotate(sprite: SKSpriteNode, direction: CGPoint, rotateRadiansPerSec: CGFloat) {
-            let shortest = shortestAngleBetween(angle1: sprite.zRotation, angle2: direction.angle)
-            let amountToRotate = min(rotateRadiansPerSec * CGFloat(deltaTime), abs(shortest))
-            sprite.zRotation += shortest.sign() * amountToRotate
         }
      
         func sceneTouched(touchLocation: CGPoint) {
