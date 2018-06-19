67d165882dc4c16e6e407a38a6eb55f83c50d201
Alexx Quan <alexxwithtwoxx@gmail.com>
2018-06-18 20:15:13 -0400

    Alexx branch tilemap

---
+++ FFive/Player.swift
@@ -0,0 +1,25 @@
+//
+//  Player.swift
+//  FFive
+//
+//  Created by Alexx Quan on 2018-06-18.
+//  Copyright © 2018 Matt Agostino. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class Player: SKSpriteNode {
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("Use init()")
+    }
+    
+    init() {
+        let texture = SKTexture(imageNamed: "hero8")
+        super.init(texture: texture, color: .white,
+                   size: texture.size())
+        name = "Player"
+        zPosition = 50
+    }
+}
