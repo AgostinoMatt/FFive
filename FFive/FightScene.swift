a98b5125f72e9fd7f149f7b87ad6556671f0b55e
matt <Agostino_matt@hotmail.com>
2018-06-20 12:31:11 -0400

    renaming

--- FFive/FightScene.swift
+++ FFive/FightScene.swift
@@ -2,38 +2,13 @@
 //  FightScene.swift
 //  FFive
 //
-//  Created by Matt Agostino on 2018-06-13.
+//  Created by Matt Agostino on 2018-06-20.
 //  Copyright © 2018 Matt Agostino. All rights reserved.
 //
 
 import SpriteKit
-import GameplayKit
-import UIKit
 
-
-class FightScene: UIViewController{
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "FightScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-                view.ignoresSiblingOrder = true
-                
-                view.showsFPS = true
-                view.showsNodeCount = true
-            }
-        }
-    }
-    
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
-    // change to fight scene controller 
+class FightScene: SKScene {
+    <#code#>
 }
+
