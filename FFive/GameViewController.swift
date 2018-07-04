f8255fe1c8b361241ee1bc3d747b6bdc72022486
matt <Agostino_matt@hotmail.com>
2018-07-04 14:15:00 -0400

    Revert "Merge branch 'newMap'"
    This reverts commit 223a22cedff4bcde685c82d1c9352cbf3aab85bc, reversing
    changes made to c6b99f5d95a3ab88637e38eabedda8e76bbd6ecf.

--- FFive/GameViewController.swift
+++ FFive/GameViewController.swift
@@ -27,7 +27,6 @@ class GameViewController: UIViewController {
                 
                 view.showsFPS = true
                 view.showsNodeCount = true
-                view.showsPhysics = true
             }
         }
     }
